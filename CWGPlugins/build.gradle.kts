bdb20acd25d82453f28031715078af5e162a8c05
Denis V <denoson@users.noreply.github.com>
2024-09-13 22:43:18 +0300

    update version 1.0.5

--- CWGPlugins/build.gradle.kts
+++ CWGPlugins/build.gradle.kts
@@ -50,7 +50,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "CarWebGuru"
             artifactId = "plugins-sdk"
-            version = "1.0.4"
+            version = "1.0.5"
             afterEvaluate { from(components["release"]) }
         }
     }
